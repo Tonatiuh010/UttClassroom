cd3a83816389d3883557452699bcb283c9927121
ElGGsus <enrikehernandex19@gmail.com>
2022-09-21 14:45:59 -0700

    service data commit

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlumnosService } from './services/alumnos.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+
+  alumnos: any = {};
+  constructor(private service: AlumnosService){
+  }
+
+  ngOnInit(): void{
+    this.service.getAllAlumnos().subscribe(resp => {
+      this.alumnos = resp.results;
+      console.log(this.alumnos); 
+    });
+  }
+
 }
